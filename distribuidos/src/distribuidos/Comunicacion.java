903e58ce13ab7a84cb2d685fd336a412250f8c3a
RAFAEL <RAFAEL@RAFAEL-PC>
2018-08-10 01:06:58 -0500

    Agregando codigos y carpetas

---
+++ distribuidos/src/distribuidos/Comunicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distribuidos;
+
+/**
+ *
+ * @author RAFAEL
+ */
+public class Comunicacion  implements Runnable{
+      @Override
+    public void run() {
+        while (true) {      
+            //FALTA AGREGAR LINEAS DE CODIGO
+            
+            try {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }catch (Exception e) {
+        System.out.println("No se puede escuchar el puerto");
+    }
+         }
+    }
+    
+}
